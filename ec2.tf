aa5046ce24f4e73b56ad276ad4e468c902191334
PM-Devops-Project <pravallika.tsm@gmail.com>
2026-01-15 12:11:32 -0800

    adding git files

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  ami                    = "ami-0ecb62995f68bb549" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
